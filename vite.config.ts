6cc7c218807ae0f1074721192402308c221c16c6
shunrong <1019486850@qq.com>
2025-08-15 19:44:46 +0800

    部署到 vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chinese-chess/', // 仓库名称作为基本路径
+  base: process.env.NODE_ENV === 'production' && process.env.VERCEL ? '/' : '/chinese-chess/',
 })
